15ea54113cea3e03aa6dc6f6482d8d1fdb78992b
Sahithi <sp3429@drexel.edu>
2018-08-30 18:32:21 -0400

    added background color and shitttt

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -12,7 +12,7 @@ export class HomePage {
 
 
   constructor(public navCtrl: NavController) {
-    this.categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5']
+    this.categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5', 'Category 6']
     this.prices = ['$100', '$200', '$300', '$400', '$500']
   }
 
