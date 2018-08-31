14679fdf5ccf245cf9bf9505c40e95ea59f42a40
lenguyentri1999 <michaelle7312@gmail.com>
2018-08-31 19:04:22 -0400

    Randomized the answers

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,11 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import { FlashCardComponent } from '../components/flash-card/flash-card';
+import { HttpClientModule } from '@angular/common/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { QuestionsProvider } from '../providers/questions/questions';
 
 @NgModule({
   declarations: [
@@ -16,7 +18,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpClientModule,
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -26,7 +29,8 @@ import { HomePage } from '../pages/home/home';
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    {provide: ErrorHandler, useClass: IonicErrorHandler},
+    QuestionsProvider
   ]
 })
 export class AppModule {}
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { QuestionsProvider } from '../../providers/questions/questions';
+import { Prompt } from '../../providers/questions/prompt';
 
 @Component({
   selector: 'page-home',
@@ -11,9 +13,43 @@ export class HomePage {
   prices: string[] = []
 
 
-  constructor(public navCtrl: NavController) {
-    this.categories = ['Category 1', 'Category 2', 'Category 3', 'Category 4', 'Category 5']
+  constructor(
+    public navCtrl: NavController,
+    public questionsPvdr: QuestionsProvider
+  ) {
+
+    this.loadCategories();
     this.prices = ['$100', '$200', '$300', '$400', '$500']
   }
 
+  loadCategories() {
+    this.questionsPvdr.getCategories().then((categories: Array<string>) => {
+      this.categories = categories;
+      this.loadPrompts(this.categories).then(shuffled => {
+        console.log('shuffled', shuffled)
+      })
+    })
+  }
+
+  async loadPrompts(categories: Array<string>): Promise<Prompt[][]> {
+    let prompts: Prompt[][] = []
+
+    for (let i = 0; i < categories.length; i++) {
+      let arr: Array<Prompt> = []
+      await this.questionsPvdr.getAnswersAndQuestions(categories, i)
+        .then((unshuffled: Array<Prompt>) => {
+          let shuffled = this.questionsPvdr.shuffleArray(unshuffled)
+          prompts.push(shuffled)
+        })
+      
+    }
+
+    return prompts
+
+  
+  }
+
+  // getPrompt(catIndex: number) {
+  //   this.questionsPvdr.getAnswersAndQuestions(catIndex).then(arr => console.log('arr', arr))
+  // }
 }
