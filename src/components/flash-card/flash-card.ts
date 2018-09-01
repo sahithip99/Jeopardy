fa577f049c944e2db94f439f557d82cb6f858b9f
Sahithi <sp3429@drexel.edu>
2018-09-01 14:45:19 -0400

    Added box shadow

--- src/components/flash-card/flash-card.ts
+++ src/components/flash-card/flash-card.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 /**
  * Generated class for the FlashCardComponent component.
@@ -10,16 +10,29 @@ import { Component } from '@angular/core';
   selector: 'flash-card',
   templateUrl: 'flash-card.html'
 })
+
 export class FlashCardComponent {
 
+  @Input('catIndex') catIndex;
+
+
   flipped: boolean = false;
+  colors: Array<string> = ['#cfa4f3fa', 'red', 'green', 'yellow', 'blue']
+  shadow: string;
 
   constructor() {
     console.log('Hello FlashCardComponent Component');
+    this.shadow = "2px 3px 5px " + this.colors[this.catIndex]
   }
 
   flip() {
     this.flipped = !this.flipped;
   }
+
+
+
+
+
+
 
 }
